dd7703f534134b10f4398713b12758bd339bf81b
Jeremy Woertink <jeremywoertink@gmail.com>
2017-04-18 13:06:27 -0700

    adding skeleton for kemal session engine

--- src/kemal-session-client-engine.cr
+++ src/kemal-session-client-engine.cr
@@ -1,3 +1,4 @@
+require "kemal-session"
 require "openssl"
 require "openssl/pkcs5"
 require "openssl/cipher"
@@ -8,8 +9,65 @@ require "json"
 require "base64"
 require "./kemal-session-client-engine/*"
 
-module Kemal
-  class Session
+class Session
+  class ClientEngine < Engine
+    class StorageInstance
+    end
+
+    def initialize(@secret_key_base : String)
+    end
+
+    def run_gc
+      # Cookies should expire by the date
+    end
     
-  end
+    def create_session(session_id : String)
+
+    end
+
+    def destroy_session(session_id : String)
+
+    end
+
+    def destroy_all_sessions
+
+    end
+
+    def all_sessions
+      
+    end
+
+    def each_session(&block)
+      yield Session.new("1")
+    end
+
+    def get_session(session_id : String)
+
+    end
+
+    macro define_delegators(vars)
+      {% for name, type in vars %}
+        def {{name.id}}(session_id : String, k : String) : {{type}}
+        end
+
+        def {{name.id}}?(session_id : String, k : String) : {{type}}?
+        end
+
+        def {{name.id}}(session_id : String, k : String, v : {{type}})
+        end
+
+        def {{name.id}}s(session_id : String) : Hash(String, {{type}})
+        end
+      {% end %}
+    end
+
+    define_delegators({
+      int: Int32,
+      bigint: Int64,
+      string: String,
+      float: Float64,
+      bool: Bool,
+      object: StorableObjects,
+    })
+  end 
 end
